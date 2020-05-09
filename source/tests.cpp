352bba6339c86f9e3f32dfa1c9ade06a9194acca
Lea <lea.hoepfner@uni-weimar.de>
2020-05-09 16:41:17 +0200

    Aufgabe 1.8 Version 1

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,5 +1,34 @@
+
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
+
+int gcd(int a, int b) {
+    int big;
+    if (a == 0 || b == 0) {
+        return 0;
+    }
+    if (a > b) {
+        big = a;
+    }
+    else {
+        big = b;
+    }
+    for (int i = big; i > 0; i--) {
+        if (a % i == 0 & b % i == 0) {
+            return i;
+            break;
+        }
+    }
+}
+
+TEST_CASE("describe_gcd", "[gcd]") {
+    REQUIRE(gcd(2, 4) == 2);
+    REQUIRE(gcd(9, 6) == 3);
+    REQUIRE(gcd(3, 7) == 1);
+    REQUIRE(gcd(4, 4) == 4);
+    REQUIRE(gcd(11, 13) == 1);
+    REQUIRE(gcd(0, 5) == 0);
+}
 
 int main(int argc, char* argv[])
 {

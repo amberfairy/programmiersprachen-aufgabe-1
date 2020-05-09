90e380d89b64e7242309df7c986e5aab4b6e6cb9
Lea <lea.hoepfner@uni-weimar.de>
2020-05-09 23:29:45 +0200

    Aufgabe 1.14 Version 1

---
+++ source/1.14.cpp
@@ -0,0 +1,32 @@
+#define CATCH_CONFIG_RUNNER
+#include <catch.hpp>
+
+bool is_prime(int zahl) {
+	bool finished = false;
+	if (zahl > 3) {
+		for (int i = 2; i < zahl; i++) {
+			if (zahl % i == 0) {
+				return false;
+				finished = true;
+				break;
+			}
+		}
+		if (finished != true) {
+			return true;
+		}
+	}
+	else {
+		return true;
+	}
+}
+
+TEST_CASE("describe_is_prime", "[is_prime]") {
+	REQUIRE(is_prime(3) == true);
+	REQUIRE(is_prime(13) == true);
+	REQUIRE(is_prime(42) == false);
+}
+
+int main(int argc, char* argv[])
+{
+	return Catch::Session().run(argc, argv);
+}
